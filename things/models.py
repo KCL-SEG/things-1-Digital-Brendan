29b3f9ef8c5657a508678fe7a4c725b5618c869f
Brendan Rogan <brendan.rogan@kcl.ac.uk>
2022-10-11 15:23:22 +0100

    Refine Thing model

--- things/models.py
+++ things/models.py
@@ -1,9 +1,10 @@
 import string
 from tokenize import String
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
 class Thing(models.Model):
-    name = ""
-    description = ""
-    quantity = int()
+    name = models.TextField(unique=True, blank=False, max_length=30)
+    description = models.TextField(unique=False, blank=True, max_length=120)
+    quantity = models.PositiveIntegerField(unique=False, validators=[MaxValueValidator(100)])
