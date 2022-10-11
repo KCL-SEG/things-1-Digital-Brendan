84ddef63889ac9b91a486189e239a96971667db4
Brendan Rogan <brendan.rogan@kcl.ac.uk>
2022-10-11 15:33:55 +0100

    Change datatypes of model attributes

--- things/models.py
+++ things/models.py
@@ -1,10 +1,10 @@
 import string
 from tokenize import String
 from django.db import models
-from django.core.validators import MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.TextField(unique=True, blank=False, max_length=30)
-    description = models.TextField(unique=False, blank=True, max_length=120)
-    quantity = models.PositiveIntegerField(unique=False, validators=[MaxValueValidator(100)])
+    name = models.CharField(unique=True, blank=False, max_length=30)
+    description = models.CharField(unique=False, blank=True, max_length=120)
+    quantity = models.PositiveIntegerField(unique=False, validators=[MaxValueValidator(100), MinValueValidator(0)])
